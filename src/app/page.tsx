51992f0d90bd52826d3f4805beb04144ac16e452
Kerry Creech <kerrywcreech@gmail.com>
2024-03-01 01:32:55 -0500

    Finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,15 @@ export default function Home() {
           width={250}
         />
       </div>
+      <div className="mt-10">
+        <p className="text-center">
+          Come learn the basics of NextJS with me! On this website I will go over what each file and directory means,
+          what the App Router is, and what Server Components are.
+        </p>
+        <p className="text-center text-4xl font-extrabold mt-10">
+          You got this!
+        </p>
+      </div>
     </main>
   );
 }
