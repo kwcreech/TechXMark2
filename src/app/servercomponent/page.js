4b5a9a6ccecc6558845291e100d9097b1bb91913
Kerry Creech <kerrywcreech@gmail.com>
2024-03-01 01:29:07 -0500

    finished server component page

--- src/app/servercomponent/page.js
+++ src/app/servercomponent/page.js
@@ -1,7 +1,12 @@
 export default function ServerComponent() {
     return (
-        <div>
-            <h1>This is the server component page</h1>
+        <div className="ml-7 mr-7">
+            <h1 className="text-center text-3xl font-extrabold mb-11">About Server Components</h1>
+            <p className="text-center">
+                While traditional React components render on both the client and server, Server Components run only on the server, leading to improved performance
+                and faster page load times. In addition, since they only run on the server, they can directly access databases and external APIs without API routes and data fetching
+                To create a Server Component, files are created with the `.server.js` extension. They work the same as React components, but are designated to only run on the server.
+            </p>
         </div>
     )
 }
